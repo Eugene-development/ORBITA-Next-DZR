3357be1d5d994e7a38104debd367453a99164122
Eugene Che <indexpro24@gmail.com>
2023-08-22 20:27:53 +0300

    c

--- project/src/app/layout/banner/store/index.js
+++ project/src/app/layout/banner/store/index.js
@@ -1,17 +1,21 @@
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
-const visibleBanner = create(
-	persist(
-		(set) => ({
-			currentVisibleBanner: true,
-			changeVisibleBanner: () => set(() => ({ currentVisibleBanner: false }))
-		}),
-		{
-			name: 'food-storage' // name of the item in the storage (must be unique)
-		}
-	)
-)
+const visibleBanner = create((set) => ({
+	currentVisibleBanner: true,
+	changeVisibleBanner: () => set(() => ({ currentVisibleBanner: false }))
+}))
+// const visibleBanner = create(
+// 	persist(
+// 		(set) => ({
+// 			currentVisibleBanner: true,
+// 			changeVisibleBanner: () => set(() => ({ currentVisibleBanner: false }))
+// 		}),
+// 		{
+// 			name: 'food-storage' // name of the item in the storage (must be unique)
+// 		}
+// 	)
+// )
 
 export const useStoreBanner = {
 	visibleBanner
