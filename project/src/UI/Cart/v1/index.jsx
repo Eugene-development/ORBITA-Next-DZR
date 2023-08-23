e527cf64aa9947a178e4fd2a46c1e6192fe15725
Eugene Che <indexpro24@gmail.com>
2023-08-23 14:54:15 +0300

    c

--- project/src/UI/Cart/v1/index.jsx
+++ project/src/UI/Cart/v1/index.jsx
@@ -2,129 +2,160 @@ import React from 'react'
 
 export default function index() {
 	return (
-		<div className="px-4 sm:px-6 lg:px-8">
-			<div className="sm:flex sm:items-center">
-				<div className="sm:flex-auto">
-					<h1 className="text-2xl font-semibold text-gray-900">Ваша корзина</h1>
-					<p className="mt-2 text-sm text-gray-700">
-						После отправки заказа с вами свяжется менеджер
-					</p>
-				</div>
-			</div>
-			<div className="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
-				<table className="min-w-full divide-y divide-gray-300 bg-gray-50">
-					<thead className="">
-						<tr>
-							<th
-								scope="col"
-								className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-							>
-								Наименование:
-							</th>
-							<th
-								scope="col"
-								className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-							>
-								Цена:
-							</th>
-							<th
-								scope="col"
-								className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
-							>
-								Цена со скидкой:
-							</th>
-
-							<th
-								scope="col"
-								className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
-							>
-								Количество:
-							</th>
-							<th
-								scope="col"
-								className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-							>
-								Итого:
-							</th>
-							<th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-								<span className="sr-only">Удалить</span>
-							</th>
-						</tr>
-					</thead>
-					<tbody className="divide-y divide-gray-200">
-						{/* <!-- {#each productsInCart as { id, value, price, unit, img, quantity }, idx} -->
-						{#each $prodInCart as { id, value, price, unit, img, quantity }, idx} */}
-						<tr>
-							<td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900  sm:w-auto sm:max-w-none sm:pl-6">
-								. <span className="ml-1 first-letter:uppercase">xxx</span>
-								<dl className="font-normal lg:hidden">
-									<dt className="sr-only">Цена</dt>
-									<dd className="mt-1 truncate">
-										<span className="inline-flex rounded-md bg-cyan-100 px-2 text-xs font-semibold leading-5 text-cyan-900">
-											Цена: xxx руб/xxx
-										</span>
-									</dd>
-									<dt className="sr-only">Цена</dt>
-									<dd className="mt-1 truncate">
-										<span className="inline-flex rounded-md bg-cyan-100 px-2 text-xs font-semibold leading-5 text-cyan-900">
-											Цена со скидкой: xxx руб/xxx
-										</span>
-									</dd>
-									<dt className="sr-only sm:hidden">Количество</dt>
-									<dd className="mt-2 mr-20 truncate text-gray-500 sm:hidden">
-										{/* <input
-												type="text"
-												className="block w-full rounded-md border-cyan-300 pr-8 focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
-												bind:value={quantity}
-											/> */}
-									</dd>
-								</dl>
-							</td>
-							<td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell ">
-								<span className="inline-flex rounded-md bg-cyan-100 px-3 py-1 text-sm font-semibold leading-5 text-cyan-900">
-									xxx руб/xxx
-								</span>
-							</td>
-							<td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell ">
-								<span className="inline-flex rounded-md bg-cyan-100 px-3 py-1 text-sm font-semibold leading-5 text-cyan-900">
-									xxx руб/xxxx
-								</span>
-							</td>
-							<td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-								{/* <input
-										type="text"
-										className="-mr-48 block w-full rounded-md border-cyan-300 pr-16 focus:border-cyan-500 focus:ring-cyan-600 sm:text-sm"
-										bind:value={quantity}
-									/> */}
-							</td>
-							<td className="px-3 py-4 text-sm text-gray-500">xxx</td>
-							<td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-								{/* <button
-										on:click={deleteProductFromCart(id)}
-										className="inline-flex items-center rounded-full border border-transparent bg-red-800 p-1.5 text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-										type="button"
+		<>
+			{true ? (
+				<div className="px-4 sm:px-6 lg:px-8">
+					<div className="sm:flex sm:items-center">
+						<div className="sm:flex-auto">
+							<h1 className="text-2xl font-semibold text-gray-900">Ваша корзина</h1>
+							<p className="mt-2 text-sm text-gray-700">
+								После отправки заказа с вами свяжется менеджер
+							</p>
+						</div>
+					</div>
+					<div className="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
+						<table className="min-w-full divide-y divide-gray-300 bg-gray-50">
+							<thead className="">
+								<tr>
+									<th
+										scope="col"
+										className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
 									>
-										<svg
-											className="h-4 w-4"
-											fill="none"
-											stroke="currentColor"
-											viewBox="0 0 24 24"
-											xmlns="http://www.w3.org/2000/svg"
+										Наименование:
+									</th>
+									<th
+										scope="col"
+										className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+									>
+										Цена:
+									</th>
+									<th
+										scope="col"
+										className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+									>
+										Цена со скидкой:
+									</th>
+
+									<th
+										scope="col"
+										className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+									>
+										Количество:
+									</th>
+									<th
+										scope="col"
+										className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+									>
+										Итого:
+									</th>
+									<th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+										<span className="sr-only">Удалить</span>
+									</th>
+								</tr>
+							</thead>
+							<tbody className="divide-y divide-gray-200">
+								{/* <!-- {#each productsInCart as { id, value, price, unit, img, quantity }, idx} -->
+							{#each $prodInCart as { id, value, price, unit, img, quantity }, idx} */}
+								<tr>
+									<td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900  sm:w-auto sm:max-w-none sm:pl-6">
+										. <span className="ml-1 first-letter:uppercase">xxx</span>
+										<dl className="font-normal lg:hidden">
+											<dt className="sr-only">Цена</dt>
+											<dd className="mt-1 truncate">
+												<span className="inline-flex rounded-md bg-cyan-100 px-2 text-xs font-semibold leading-5 text-cyan-900">
+													Цена: xxx руб/xxx
+												</span>
+											</dd>
+											<dt className="sr-only">Цена</dt>
+											<dd className="mt-1 truncate">
+												<span className="inline-flex rounded-md bg-cyan-100 px-2 text-xs font-semibold leading-5 text-cyan-900">
+													Цена со скидкой: xxx руб/xxx
+												</span>
+											</dd>
+											<dt className="sr-only sm:hidden">Количество</dt>
+											<dd className="mt-2 mr-20 truncate text-gray-500 sm:hidden">
+												{/* <input
+													type="text"
+													className="block w-full rounded-md border-cyan-300 pr-8 focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
+													bind:value={quantity}
+												/> */}
+											</dd>
+										</dl>
+									</td>
+									<td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell ">
+										<span className="inline-flex rounded-md bg-cyan-100 px-3 py-1 text-sm font-semibold leading-5 text-cyan-900">
+											xxx руб/xxx
+										</span>
+									</td>
+									<td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell ">
+										<span className="inline-flex rounded-md bg-cyan-100 px-3 py-1 text-sm font-semibold leading-5 text-cyan-900">
+											xxx руб/xxxx
+										</span>
+									</td>
+									<td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+										{/* <input
+											type="text"
+											className="-mr-48 block w-full rounded-md border-cyan-300 pr-16 focus:border-cyan-500 focus:ring-cyan-600 sm:text-sm"
+											bind:value={quantity}
+										/> */}
+									</td>
+									<td className="px-3 py-4 text-sm text-gray-500">xxx</td>
+									<td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+										{/* <button
+											on:click={deleteProductFromCart(id)}
+											className="inline-flex items-center rounded-full border border-transparent bg-red-800 p-1.5 text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+											type="button"
 										>
-											<path
-												d="M6 18L18 6M6 6l12 12"
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-											/>
-										</svg>
-									</button> */}
-							</td>
-						</tr>
-						{/* {/each} */}
-					</tbody>
-				</table>
-			</div>
-		</div>
+											<svg
+												className="h-4 w-4"
+												fill="none"
+												stroke="currentColor"
+												viewBox="0 0 24 24"
+												xmlns="http://www.w3.org/2000/svg"
+											>
+												<path
+													d="M6 18L18 6M6 6l12 12"
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													stroke-width="2"
+												/>
+											</svg>
+										</button> */}
+									</td>
+								</tr>
+								{/* {/each} */}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			) : (
+				<div className="m-8">
+					<div className="rounded-md bg-blue-50 p-4">
+						<div className="flex ">
+							<div className="flex-shrink-0">
+								<svg
+									aria-hidden="true"
+									className="h-5 w-5 text-blue-400"
+									fill="currentColor"
+									viewBox="0 0 20 20"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										clip-rule="evenodd"
+										d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+										fillRule="evenodd"
+									/>
+								</svg>
+							</div>
+							<div className="ml-3 flex-1 md:flex md:justify-between">
+								<p className="text-sm text-cyan-700">
+									Корзина пуста. Ждём вас снова за покупками.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
 	)
 }
--- project/src/UI/Category/v1/index.jsx
+++ project/src/UI/Category/v1/index.jsx
@@ -7,10 +7,10 @@ const frequencies = [
 	{ value: 'monthly', label: 'Monthly', priceSuffix: '/month' },
 	{ value: 'annually', label: 'Annually', priceSuffix: '/year' }
 ]
-const tiers = [
+const items = [
 	{
 		name: 'Доска обрезная обработанная',
-		id: 'tier-freelancer',
+		id: 'item-freelancer',
 		href: '#',
 		price: { monthly: '$15', annually: '$144' },
 		description: 'The essentials to provide your best work for clients.',
@@ -24,7 +24,7 @@ const tiers = [
 	},
 	{
 		name: 'Доска обрезная обработанная',
-		id: 'tier-startup',
+		id: 'item-startup',
 		href: '#',
 		price: { monthly: '$30', annually: '$288' },
 		description: 'A plan that scales with your rapidly growing business.',
@@ -33,7 +33,7 @@ const tiers = [
 	},
 	{
 		name: 'Enterprise',
-		id: 'tier-enterprise',
+		id: 'item-enterprise',
 		href: '#',
 		price: { monthly: '$60', annually: '$576' },
 		description: 'Dedicated support and infrastructure for your company.',
@@ -70,40 +70,40 @@ export default function Example({ rubric }) {
 					или самовывозом с нашего склада
 				</p>
 				<div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-					{rubric.rubric_one.category.map((tier) => (
+					{rubric.rubric_one.category.map((item) => (
 						<div
-							key={tier.id}
+							key={item.value}
 							className={classNames(
-								tier.mostPopular ? 'ring-2 ring-red-600' : 'ring-1 ring-gray-200',
+								item.mostPopular ? 'ring-2 ring-red-600' : 'ring-1 ring-gray-200',
 								'rounded-3xl p-6 xl:p-8'
 							)}
 						>
 							<div className="flex items-center justify-between gap-x-4">
 								<h2
-									id={tier.id}
+									id={item.id}
 									className={classNames(
-										tier.mostPopular ? 'text-red-600' : 'text-gray-900',
+										item.mostPopular ? 'text-red-600' : 'text-gray-900',
 										'text-sm font-bold leading-8'
 									)}
 								>
-									{tier.value}
+									{item.value}
 								</h2>
-								{/* {tier.mostPopular ? (
+								{/* {item.mostPopular ? (
                   <p className="rounded-full bg-red-600/10 px-2.5 py-1 text-xs font-semibold leading-5 text-red-600">
                     Most popular
                   </p>
                 ) : null} */}
 							</div>
-							{/* <p className="mt-4 text-sm leading-6 text-gray-600">{tier.description}</p> */}
+							{/* <p className="mt-4 text-sm leading-6 text-gray-600">{item.description}</p> */}
 							{/* <p className="mt-6 flex items-baseline gap-x-1">
-                <span className="text-4xl font-bold tracking-tight text-gray-900">{tier.price[frequency.value]}</span>
+                <span className="text-4xl font-bold tracking-tight text-gray-900">{item.price[frequency.value]}</span>
                 <span className="text-sm font-semibold leading-6 text-gray-600">{frequency.priceSuffix}</span>
               </p> */}
 							<a
-								href={tier.href}
-								aria-describedby={tier.id}
+								href={item.href}
+								aria-describedby={item.id}
 								className={classNames(
-									tier.mostPopular
+									item.mostPopular
 										? 'bg-red-600 text-white shadow-sm hover:bg-red-500'
 										: 'text-red-600 ring-1 ring-inset ring-red-200 hover:ring-red-300',
 									'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600'
@@ -115,7 +115,7 @@ export default function Example({ rubric }) {
 								role="list"
 								className="mt-8 space-y-3 text-sm leading-6 text-gray-600 xl:mt-10"
 							>
-								{/* {tier.features.map((feature) => (
+								{/* {item.features.map((feature) => (
 									<li key={feature} className="flex gap-x-3">
 										<CheckIcon
 											className="h-6 w-5 flex-none text-red-600"
