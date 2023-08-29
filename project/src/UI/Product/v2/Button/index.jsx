edbb2ca83b19570613b263d1315676da604af97b
Eugene Che <indexpro24@gmail.com>
2023-08-29 19:16:04 +0300

    c

--- project/src/UI/Product/v2/Button/index.jsx
+++ project/src/UI/Product/v2/Button/index.jsx
@@ -6,9 +6,15 @@ export default () => {
 	// const { setCurrentProductInCart } = inCart()
 	const { productInCart, changeStatusButton } = inCart()
 
+    const handleClick = (event) => {
+    event.preventDefault(); 
+    changeStatusButton();
+  };
+
     
   return (
     <button
+        onClick={handleClick}
         type="submit"
         className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
     >
