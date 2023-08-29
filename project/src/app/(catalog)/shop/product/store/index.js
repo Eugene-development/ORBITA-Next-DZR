4bf3c033542252bbc1c539d8b9719c0d00e12f67
Eugene Che <indexpro24@gmail.com>
2023-08-29 20:31:47 +0300

    c

--- project/src/app/(catalog)/shop/product/store/index.js
+++ project/src/app/(catalog)/shop/product/store/index.js
@@ -1,19 +1,13 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
-// import { immer } from 'zustand/middleware/immer'
 const inCart = create(
 	persist(
-		// immer(
 		(set) => ({
 			productsInCart: [],
 			setCurrentProductInCart: (id) =>
 				set((state) => ({
 					productsInCart: [...state.productsInCart, id]
 				}))
-			// setCurrentProductInCart: (id) =>
-			// 	set((state) => {
-			// 		state.productsInCart.push({ id })
-			// 	})
 			// setCurrentProductInCart: (id) => set(() => ({ productsInCart: id }))
 			// productInCart: false,
 			// changeStatusButton: () => set(() => ({ productInCart: true }))
@@ -22,7 +16,6 @@ const inCart = create(
 			name: 'cart'
 		}
 	)
-	// )
 )
 
 export const useInCart = {
