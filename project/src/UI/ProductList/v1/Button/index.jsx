dd09cfad670e8028c43c916f2580dcdad323a953
Eugene Che <indexpro24@gmail.com>
2023-09-06 11:30:24 +0300

    c

--- project/src/UI/ProductList/v1/Button/index.jsx
+++ project/src/UI/ProductList/v1/Button/index.jsx
@@ -22,14 +22,14 @@ export default ({ productID }) => {
 	return (
 		<>
 			{isClient && productsInCart.includes(productID) ? (
-				<div className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-900 px-8 py-3 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
+				<div className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-900 px-4 py-3 text-base font-medium text-white  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">
 					<span>В корзине</span>
 				</div>
 			) : (
 				<button
 					onClick={handleClick}
 					type="button"
-					className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-500 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+					className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
 				>
 					<span>В корзину</span>
 				</button>
