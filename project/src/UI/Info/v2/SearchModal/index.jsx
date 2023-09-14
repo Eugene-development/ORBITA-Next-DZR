c44c4d1cdfd35f937636b31ec7902d471156038f
Eugene Che <indexpro24@gmail.com>
2023-09-14 22:04:38 +0300

    с

--- project/src/UI/Info/v2/SearchModal/index.jsx
+++ project/src/UI/Info/v2/SearchModal/index.jsx
@@ -43,10 +43,10 @@ export default function Example() {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-5xl sm:p-6">
                 
                 
-        <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased">
-            <div className="mx-auto max-w-screen-2xl px-1 lg:px-2">
+        <section className=" dark:bg-gray-900 p-3 sm:p-5 antialiased">
+            <div className="mx-auto max-w-screen-2xl">
                 <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
-                    <div className="flex flex-col md:flex-row md:items-center md:justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                    {/* <div className="flex flex-col md:flex-row md:items-center md:justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                         <div className="flex-1 flex items-center space-x-2">
                             <h5>
                                 <span className="text-gray-500">All Products:</span>
@@ -64,18 +64,18 @@ export default function Example() {
                                 <div className="tooltip-arrow" data-popper-arrow=""></div>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex flex-col md:flex-row items-stretch md:items-center md:space-x-3 space-y-3 md:space-y-0 justify-between mx-4 py-4 border-t dark:border-gray-700">
+                    </div> */}
+                    <div className="flex flex-col md:flex-row items-stretch md:items-center md:space-x-3 space-y-3 md:space-y-0 justify-between mx-4 py-4  dark:border-gray-700">
                         <div className="w-full md:w-1/2">
                             <form className="flex items-center">
-                                <label htmlFor="simple-search" className="sr-only">Search</label>
+                                <label htmlFor="simple-search" className="sr-only">Поиск</label>
                                 <div className="relative w-full">
                                     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                         <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path fillRule="evenodd" clipRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
                                         </svg>
                                     </div>
-                                    <input type="text" id="simple-search" placeholder="Search for products" required="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"/>
+                                    <input type="text" id="simple-search" placeholder="Поиск по каталогу" required="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"/>
                                 </div>
                             </form>
                         </div>
@@ -89,7 +89,7 @@ export default function Example() {
                                     </div>
                                 </div>
                                 <div className="pt-3 pb-2">
-                                    <label htmlFor="input-group-search" className="sr-only">Search</label>
+                                    <label htmlFor="input-group-search" className="sr-only">Поиск</label>
                                     <div className="relative">
                                         <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
                                             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -417,114 +417,6 @@ export default function Example() {
                 </button>
             </div>
         </form> */}
-        {/* <!-- Preview Drawer --> */}
-        <div id="drawer-read-product-advanced" className="overflow-y-auto fixed top-0 left-0 z-40 p-4 w-full max-w-lg h-screen bg-white transition-transform -translate-x-full dark:bg-gray-800" tabIndex="-1" aria-labelledby="drawer-label" aria-hidden="true">
-            <div>
-                <h4 id="read-drawer-label" className="mb-1.5 leading-none text-xl font-semibold text-gray-900 dark:text-white">Apple iMac 25"</h4>
-                <h5 className="mb-5 text-xl font-bold text-gray-900 dark:text-white">$2999</h5>
-            </div>
-            <button type="button" data-drawer-dismiss="drawer-read-product-advanced" aria-controls="drawer-read-product-advanced" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
-                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                </svg>
-                <span className="sr-only">Close menu</span>
-            </button>
-            <div className="grid grid-cols-3 gap-4 mb-4 sm:mb-5">
-                <div className="p-2 w-auto bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-side-image.png" alt="iMac Side Image"/>
-                </div>
-                <div className="p-2 w-auto bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-front-image.png" alt="iMac Front Image"/>
-                </div>
-                <div className="p-2 w-auto bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-back-image.png" alt="iMac Back Image"/>
-                </div>
-                <div className="p-2 w-auto bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-side-image.png" alt="iMac Back Image"/>
-                </div>
-                <div className="p-2 w-auto bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-back-image.png" alt="iMac Front Image"/>
-                </div>
-                <div className="p-2 w-auto bg-gray-100 rounded-lg dark:bg-gray-700">
-                    <img src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-side-image.png" alt="iMac Side Image"/>
-                </div>
-            </div>
-            <dl className="sm:mb-5"><dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Details</dt><dd className="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">Standard glass ,3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US.</dd></dl>
-            <dl className="grid grid-cols-2 gap-4 mb-4">
-                <div className="col-span-2 p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 sm:col-span-1 dark:border-gray-600">
-                    <dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Shipping</dt>
-                    <dd className="flex items-center text-gray-500 dark:text-gray-400">
-                        <svg className="w-4 h-4 mr-1.5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
-                        </svg>
-                        United States, Europe
-                    </dd>
-                </div>
-                <div className="col-span-2 p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 sm:col-span-1 dark:border-gray-600">
-                    <dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Colors</dt>
-                    <dd className="flex items-center space-x-2 font-light text-gray-500 dark:text-gray-400">
-                        <div className="flex-shrink-0 w-6 h-6 bg-purple-600 rounded-full"></div>
-                        <div className="flex-shrink-0 w-6 h-6 bg-indigo-400 rounded-full"></div>
-                        <div className="flex-shrink-0 w-6 h-6 rounded-full bg-primary-600"></div>
-                        <div className="flex-shrink-0 w-6 h-6 bg-pink-400 rounded-full"></div>
-                        <div className="flex-shrink-0 w-6 h-6 bg-teal-300 rounded-full"></div>
-                        <div className="flex-shrink-0 w-6 h-6 bg-green-300 rounded-full"></div>
-                    </dd>
-                </div>
-                <div className="p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-                    <dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Product State</dt>
-                    <dd className="text-gray-500 dark:text-gray-400">
-                        <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                            <svg aria-hidden="true" className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                            </svg>
-                            New
-                        </span>
-                    </dd>
-                </div>
-                <div className="p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600"><dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Sold by</dt><dd className="text-gray-500 dark:text-gray-400">Flowbite</dd></div>
-                <div className="p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600"><dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Ships from</dt><dd className="text-gray-500 dark:text-gray-400">Flowbite</dd></div>
-                <div className="p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600"><dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Brand</dt><dd className="text-gray-500 dark:text-gray-400">Apple</dd></div>
-                <div className="p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600"><dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Dimensions (cm)</dt><dd className="text-gray-500 dark:text-gray-400">105 x 15 x 23</dd></div>
-                <div className="p-3 bg-gray-100 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600"><dt className="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Item weight</dt><dd className="text-gray-500 dark:text-gray-400">12kg</dd></div>
-            </dl>
-            <div className="flex bottom-0 left-0 justify-center pb-4 space-x-4 w-full">
-                <button type="button" className="text-white w-full inline-flex items-center justify-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    <svg aria-hidden="true" className="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                        <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
-                    </svg>
-                    Edit
-                </button>
-                <button type="button" className="inline-flex w-full items-center text-white justify-center bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
-                    <svg aria-hidden="true" className="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" />
-                    </svg>
-                    Delete
-                </button>
-            </div>
-        </div>
-        {/* <!-- Delete Modal --> */}
-        <div id="delete-modal" tabIndex="-1" className="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div className="relative w-full h-auto max-w-md max-h-full">
-                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="delete-modal">
-                        <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                        </svg>
-                        <span className="sr-only">Close modal</span>
-                    </button>
-                    <div className="p-6 text-center">
-                        <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                        </svg>
-                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                        <button data-modal-toggle="delete-modal" type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Yes, I'm sure</button>
-                        <button data-modal-toggle="delete-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
-                    </div>
-                </div>
-            </div>
-        </div>
                 
                 
                 
@@ -538,21 +430,14 @@ export default function Example() {
                 
                 
                 
-                <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                  <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
-                    onClick={() => setOpen(false)}
-                  >
-                    Deactivate
-                  </button>
+                <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
                     className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                    onClick={() => setOpen(false)}
+                    onClick={closeVisibleSearch}
                     ref={cancelButtonRef}
                   >
-                    Cancel
+                    Закрыть
                   </button>
                 </div>
               </Dialog.Panel>
