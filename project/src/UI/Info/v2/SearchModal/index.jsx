23f82d78109eca26411eb186a3d7beb911c89d01
Eugene Che <indexpro24@gmail.com>
2023-09-14 22:16:02 +0300

    c

--- project/src/UI/Info/v2/SearchModal/index.jsx
+++ project/src/UI/Info/v2/SearchModal/index.jsx
@@ -43,9 +43,9 @@ export default function Example() {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-5xl sm:p-6">
                 
                 
-        <section className=" dark:bg-gray-900 p-3 sm:p-5 antialiased">
-            <div className="mx-auto max-w-screen-2xl">
-                <div className="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+        <section className=" dark:bg-gray-900 antialiased">
+            <div className="mx-auto max-w-screen-3xl">
+                <div className="bg-white dark:bg-gray-800 relative border rounded-md">
                     {/* <div className="flex flex-col md:flex-row md:items-center md:justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
                         <div className="flex-1 flex items-center space-x-2">
                             <h5>
@@ -104,7 +104,7 @@ export default function Example() {
                     </div>
                     <div className="overflow-x-auto">
                         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase border-b dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" className="p-4">Наименование</th>
                                     <th scope="col" className="p-4">Категория</th>
@@ -113,6 +113,29 @@ export default function Example() {
                                 </tr>
                             </thead>
                             <tbody>
+                                <tr className="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
+                                    <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <div className="flex items-center mr-3">
+                                            <img src="https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-front-image.png" alt="iMac Front Image" className="h-8 w-auto mr-3"/>
+                                            Apple iMac 27&#34;
+                                        </div>
+                                    </th>
+                                    <td className="px-4 py-3">
+                                        <span className="bg-primary-100 text-primary-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-primary-900 dark:text-primary-300">Desktop PC</span>
+                                    </td>
+                                    <td className="px-4 py-3">$3.2M</td>
+                                    <td className="px-4 py-3  font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        <div className="flex items-center space-x-4 ">
+                                            <button type="button" data-drawer-target="drawer-read-product-advanced" data-drawer-show="drawer-read-product-advanced" aria-controls="drawer-read-product-advanced" className="py-2 px-3 flex items-center mx-auto text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 mr-2 -ml-0.5">
+                                                    <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                                    <path fillRule="evenodd" clipRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" />
+                                                </svg>
+                                                В корзину
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
                                 <tr className="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
                                     <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         <div className="flex items-center mr-3">
