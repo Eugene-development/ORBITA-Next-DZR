c9da48e41b575e999654fffa7e1f98f1e3cd656b
Eugene Che <indexpro24@gmail.com>
2023-09-17 17:22:54 +0300

    c

--- project/src/UI/Map/v1/index.jsx
+++ project/src/UI/Map/v1/index.jsx
@@ -6,7 +6,6 @@ export default function index() {
 				src="https://yandex.ru/map-widget/v1/?um=constructor%3Aca61ff22a5f3ccf0758454d96fa4428e6873fcc111a287ffc09faaef74d17234&amp;source=constructor"
 				width="1174"
 				height="461"
-				frameborder="0"
 				title="Map"
 			/>
 		</div>
