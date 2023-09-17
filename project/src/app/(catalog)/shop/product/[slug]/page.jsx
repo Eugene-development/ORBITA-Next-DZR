4afc760237a1b1dd410b48591547c02647515b85
Eugene Che <indexpro24@gmail.com>
2023-09-17 16:51:59 +0300

    c

--- project/src/app/(catalog)/shop/product/[slug]/page.jsx
+++ project/src/app/(catalog)/shop/product/[slug]/page.jsx
@@ -1,14 +1,16 @@
 import Page from '../DAL'
+import { getOneProduct } from '../server'
 
 
-export async function generateMetadata() {
-  const catalog = await getCatalog()
+
+// export async function generateMetadata() {
+//   const catalog = await getOneProduct()
  
-  return {
-    title: catalog.product_one.value,
+//   return {
+//     title: catalog.product_one.value,
     
-  }
-}
+//   }
+// }
 export default ({ params }) => {
 	return <Page params={params} />
 }
--- project/src/app/(catalog)/shop/rubric/[slug]/page.jsx
+++ project/src/app/(catalog)/shop/rubric/[slug]/page.jsx
@@ -1,6 +1,15 @@
 import { Rubric } from '@/UI'
 import { getOneCatalog } from '../server'
 
+
+export async function generateMetadata({ params }) {
+  const catalog = await getOneCatalog(params)
+ 
+  return {
+    title: catalog.catalog_one.value,
+    
+  }
+}
 export default async ({ params }) => {
 	const catalog = await getOneCatalog(params)
 
